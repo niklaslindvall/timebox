2fd88551def6b6b8cb9315694f5699c6aeaa7a7f
Peter Hardy <peter@hardy.dropbear.id.au>
2017-12-09 22:30:47 +1100

    typo

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='timebox',
       version='0.1',
-      package_dir = {'': 'package'}
+      package_dir = {'': 'package'},
       py_modules=['timebox', 'timeboximage', 'messages', 'utils/fonts', 'utils/gifreader'],
       )
